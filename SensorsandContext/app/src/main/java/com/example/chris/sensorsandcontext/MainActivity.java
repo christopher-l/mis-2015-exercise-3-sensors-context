9d6b3af13df82e8675c6676e7e22e5ae7d8a59e9
Christopher Lübbemeier <christopher.luebbemeier@gmail.com>
2015-05-20 15:30:15 +0200

    create 2 fragments

--- SensorsandContext/app/src/main/java/com/example/chris/sensorsandcontext/MainActivity.java
+++ SensorsandContext/app/src/main/java/com/example/chris/sensorsandcontext/MainActivity.java
@@ -126,15 +126,18 @@ public class MainActivity extends Activity implements ActionBar.TabListener {
 
         @Override
         public Fragment getItem(int position) {
-            // getItem is called to instantiate the fragment for the given page.
-            // Return a PlaceholderFragment (defined as a static inner class below).
-            return PlaceholderFragment.newInstance(position + 1);
+            switch (position) {
+                case 0:
+                    return new Fragment1();
+                case 1:
+                    return new Fragment2();
+            }
+            return null;
         }
 
         @Override
         public int getCount() {
-            // Show 3 total pages.
-            return 3;
+            return 2;
         }
 
         @Override
@@ -145,36 +148,15 @@ public class MainActivity extends Activity implements ActionBar.TabListener {
                     return getString(R.string.title_section1).toUpperCase(l);
                 case 1:
                     return getString(R.string.title_section2).toUpperCase(l);
-                case 2:
-                    return getString(R.string.title_section3).toUpperCase(l);
             }
             return null;
         }
     }
 
-    /**
-     * A placeholder fragment containing a simple view.
-     */
-    public static class PlaceholderFragment extends Fragment {
-        /**
-         * The fragment argument representing the section number for this
-         * fragment.
-         */
-        private static final String ARG_SECTION_NUMBER = "section_number";
 
-        /**
-         * Returns a new instance of this fragment for the given section
-         * number.
-         */
-        public static PlaceholderFragment newInstance(int sectionNumber) {
-            PlaceholderFragment fragment = new PlaceholderFragment();
-            Bundle args = new Bundle();
-            args.putInt(ARG_SECTION_NUMBER, sectionNumber);
-            fragment.setArguments(args);
-            return fragment;
-        }
+    public static class Fragment1 extends Fragment {
 
-        public PlaceholderFragment() {
+        public Fragment1() {
         }
 
         @Override
@@ -184,5 +166,19 @@ public class MainActivity extends Activity implements ActionBar.TabListener {
             return rootView;
         }
     }
+
+    public static class Fragment2 extends Fragment {
+
+        public Fragment2() {
+        }
+
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                                 Bundle savedInstanceState) {
+            View rootView = inflater.inflate(R.layout.fragment_main_2, container, false);
+            return rootView;
+        }
+    }
+
 
 }
